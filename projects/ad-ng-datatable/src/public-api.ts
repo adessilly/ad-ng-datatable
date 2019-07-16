d1683532c9d6da00bc89c63b26aee63f13cbb8b9
Adrien Dessilly <Adrien.Dessilly@civadis.be>
2019-07-16 15:02:37 +0200

    angular v8 fork of ng-datatable by PascalHonegger

---
+++ projects/ad-ng-datatable/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of ad-ng-datatable
+ */
+
+export * from './lib/DataTableModule';
+export * from './lib/BootstrapPaginator';
+export * from './lib/DataTable';
+export * from './lib/DefaultSorter';
+export * from './lib/Paginator';
